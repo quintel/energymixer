eb29b3bbd7b35b3543aa0c5fb856f67a58db2581
Paolo Zaccagnini <paozac@gmail.com>
2011-03-04 11:41:49 +0100

    added_indexes, nested attributes on inputs

--- db/migrate/20110304091407_create_inputs.rb
+++ db/migrate/20110304091407_create_inputs.rb
@@ -2,11 +2,13 @@ class CreateInputs < ActiveRecord::Migration
   def self.up
     create_table :inputs do |t|
       t.string :key
-      t.decimal :value
-      t.integer :answr_id
+      t.decimal :value, :precision => 10, :scale => 2      
+      t.integer :answer_id
 
       t.timestamps
     end
+    
+    add_index :inputs, :answer_id
   end
 
   def self.down
