663735fd3d93827081c021e262f4a8dd5bdc0558
Paolo Zaccagnini <paozac@gmail.com>
2011-03-04 15:29:14 +0100

    seeds file with questions, added answer description column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110304091407) do
+ActiveRecord::Schema.define(:version => 20110304142404) do
 
   create_table "answers", :force => true do |t|
     t.string   "answer"
@@ -18,7 +18,10 @@ ActiveRecord::Schema.define(:version => 20110304091407) do
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
+
+  add_index "answers", ["question_id"], :name => "index_answers_on_question_id"
 
   create_table "inputs", :force => true do |t|
     t.string   "key"
