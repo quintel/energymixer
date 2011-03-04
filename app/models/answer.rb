227a472cdc69ad5ce7ad35cd9f660d37951bcfea
Paolo Zaccagnini <paozac@gmail.com>
2011-03-04 14:35:12 +0100

    renamed home controller spec file

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :inputs, :dependent => :destroy
   
-  accepts_nested_attributes_for :inputs,
-    :allow_destroy => true, :reject_if => proc {|attrs| attrs['key'].blank? }
+  accepts_nested_attributes_for :inputs, :allow_destroy => true, :reject_if => proc {|attrs| attrs['key'].blank? }
 
   validates :answer, :presence => true
 end
