b3d460fc33f4eaa30b0a1f0692aa866d07950b3d
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-07 21:50:06 +0100

    added letter for answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,8 +3,13 @@ class Answer < ActiveRecord::Base
   has_many :inputs, :dependent => :destroy
   
   accepts_nested_attributes_for :inputs, :allow_destroy => true, :reject_if => proc {|attrs| attrs['key'].blank? }
-
+  
   validates :answer, :presence => true
   
-  scope :ordered, order('ordering, id')  
+  scope :ordered, order('ordering, id')
+  
+  def letter
+    ('A'..'Z').to_a[ordering]
+  end
+  
 end
