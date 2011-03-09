3eb0797f7d3d10793a5f0eee7c49922c45c7c33a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-09 15:55:28 +0100

    added link hooks but still showing all questions

---
+++ public/javascripts/questions.navigation.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#next_question").click(function(){
+    console.log("next clicked");
+  });
+  $("#previous_question").click(function(){
+    console.log("previous clicked");
+  });
+});
