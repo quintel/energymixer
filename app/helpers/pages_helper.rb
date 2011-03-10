62a1cd2710230b046ae7870b0ab4b016e7716552
Paolo Zaccagnini <paozac@gmail.com>
2011-03-10 15:56:15 +0100

    store answers as a json through a simple view helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,4 +8,20 @@ module PagesHelper
     end
   end
 
+  def answers_json
+    out = {}
+    Question.all.each do |q|
+      q_id = "question_#{q.id}"
+      out[q_id] = {}
+      q.answers.each do |a|
+        a_id = "answer_#{a.id}"
+        out[q_id][a_id] = {}
+        a.inputs.each do |i|
+          out[q_id][a_id][i.id] = i.value
+        end
+      end
+    end
+    out.to_json
+  end
+
 end
