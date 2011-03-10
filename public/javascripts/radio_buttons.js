30a0460379d2a901acd85b4b01e05a9b3f8b5ece
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-10 21:14:38 +0100

    removing active class from sibling-answers in a question

--- public/javascripts/radio_buttons.js
+++ public/javascripts/radio_buttons.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
   $("input[type='radio']").change(function(){
-      var id = $(this).attr('id');
-      //console.log('Question changed to answer id:' + id + ' changed.');
-      mixer.refresh();
-      $("label[for=" + id + "]").addClass('active');
+    var id = $(this).attr('id');
+    //console.log('Question changed to answer id:' + id + ' changed.');
+    mixer.refresh();
+    $(this).parent().parent().find("label").removeClass('active');
+    $("label[for=" + id + "]").addClass('active');
   });
+
+  $("input[type='radio']").focus(function(){
+    alert('focus!');
+  }).blur(function(){
+    alert('blur!');
+  });
+
 });
