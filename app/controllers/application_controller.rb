a5dde50dc3fad14ec2fa56d86e6239cf633fe9f0
Neil L. Decapia <neildecapia@gmail.com>
2011-03-12 00:16:26 +0800

    Adding (disabled for now) admin sign in and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  # NOTE uncomment this when you're ready for prime time
+  # before_filter :authenticate_user!
+
   protect_from_forgery
 
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
-  protected
-  
-    def record_not_found
-      flash[:alert] = 'Record not found'
-      redirect_to :back
-    rescue
-      redirect_to root_path
-    end  
+protected
+
+  def record_not_found
+    flash[:alert] = 'Record not found'
+    redirect_to :back
+  rescue
+    redirect_to root_path
+  end
+
+  # Overridden to send admins to the questions index automatically
+  def after_sign_in_path_for(resource)
+    questions_path
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
+  skip_before_filter :authenticate_user!
+
   def home
     @questions = Question.ordered.all
     
