a3c32e2975a3b68d798882b4a670d476971531f7
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-11 12:28:45 +0100

    remove results from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110311092607) do
+ActiveRecord::Schema.define(:version => 20110311111008) do
 
   create_table "answers", :force => true do |t|
     t.string   "answer"
@@ -43,15 +43,6 @@ ActiveRecord::Schema.define(:version => 20110311092607) do
   end
 
   add_index "questions", ["ordering"], :name => "index_questions_on_ordering"
-
-  create_table "results", :force => true do |t|
-    t.string   "gquery"
-    t.string   "key"
-    t.string   "description"
-    t.string   "group"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
