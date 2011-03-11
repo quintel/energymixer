537c0d8e567758904f3aee1e4a016c8c0e2ffc1e
Paolo Zaccagnini <paozac@gmail.com>
2011-03-11 17:27:43 +0100

    removed xml support created by scaffold, it wasn't required

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,30 +2,15 @@ class QuestionsController < ApplicationController
   layout 'admin'
   def index
     @questions = Question.ordered.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @questions }
-    end
   end
 
   def show
     @question = Question.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @question }
-    end
   end
 
   def new
     @question = Question.new
     5.times{ @question.answers.build }
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @question }
-    end
   end
 
   def edit
@@ -36,38 +21,25 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(params[:question])
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to(@question, :notice => 'Question was successfully created.') }
-        format.xml  { render :xml => @question, :status => :created, :location => @question }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+    if @question.save
+      redirect_to(@question, :notice => 'Question was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   def update
     @question = Question.find(params[:id])
-
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        format.html { redirect_to(@question, :notice => 'Question was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+    if @question.update_attributes(params[:question])
+      redirect_to(@question, :notice => 'Question was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(questions_url, :notice => 'Question deleted') }
-      format.xml  { head :ok }
-    end
+    redirect_to(questions_url, :notice => 'Question deleted')
   end
 end
