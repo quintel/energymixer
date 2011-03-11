07ede69f484b5269840901a1bf0f68524c04ada3
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-11 15:27:31 +0100

    more navigation for questions (up you can now select directly the question

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
-  //probably move this to the html, because it might show in the interface when page loads slowly, DS
-  $(".question").not("#question_1").hide();
+
+  /*
+  //  Navigating through the questions
+  */
+
+  $(".question").not("#question_1").hide();   //probably move this to the html, because it might show in the interface when page loads slowly, DS
   $("#question_1").addClass('active_question');
 
   $("#next_question").click(function(){
     $(".active_question").removeClass('active_question').hide().next().addClass('active_question').show();
     return false;
   });
+  
   $("#previous_question").click(function(){
     $(".active_question").removeClass('active_question').hide().prev().addClass('active_question').show();
     return false;
   });
+  
+  $("#questions nav#up a").click(function(){
+    $(".question").hide().removeClass('active_question');
+    $("#" + $(this).attr('rel') ).show().addClass('active_question');
+    return false;
+  });
 
-
+  /*
+  //  Changing labels
+  */
   $("input[type='radio']").change(function(){
     var id = $(this).attr('id');
-    //console.log('Question changed to answer id:' + id + ' changed.');
     mixer.refresh();
     $(this).parent().parent().find("label").removeClass('active');
     $("label[for=" + id + "]").addClass('active');
