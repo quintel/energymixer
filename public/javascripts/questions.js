1596f6abb7e2c2d0ce844d0a148a502d36bd1ecc
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-11 15:05:05 +0100

    questions are hidden and previous next buttons work.

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+  //probably move this to the html, because it might show in the interface when page loads slowly, DS
+  $(".question").not("#question_1").hide();
+  $("#question_1").addClass('active_question');
+
   $("#next_question").click(function(){
-    console.log("next clicked");
+    $(".active_question").removeClass('active_question').hide().next().addClass('active_question').show();
+    return false;
   });
   $("#previous_question").click(function(){
-    console.log("previous clicked");
+    $(".active_question").removeClass('active_question').hide().prev().addClass('active_question').show();
+    return false;
   });
 
 
