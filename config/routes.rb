1a4185eb10cfbf1d27cf6d24d60674ba3d42ab6a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-11 12:05:51 +0100

    Removed result model controller etc.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ EnergyMixer::Application.routes.draw do
   resources :inputs
   resources :answers, :only => [:edit, :update, :show]
   resources :questions
-  resources :results
 
   match "/home" => "pages#home"
   match "/mix" => "pages#mix"
