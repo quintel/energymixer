f5d364a29c2d37bdc0ffbaed2fcebb63c265cb5a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-14 17:50:23 +0100

    new gqueries for column

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,35 +5,35 @@ class PagesController < ApplicationController
   def home
     @questions = Question.ordered.all
     
-    @results = {
+    @results = {    
       mix: {
         coal: {
-          gquery: "total_cost_of_primary_coal",
+          gquery: "costs_share_of_coal",
           unit: "Mln. Euro"
         },
         natural_gas: {
-          gquery: "total_cost_of_primary_natural_gas",
+          gquery: "costs_share_of_gas",
           unit: "Mln. Euro"
         }, 
         total_cost_of_primary_oil: {
-          gquery: "total_cost_of_primary_oil",
+          gquery: "costs_share_of_oil",
           unit: "Mln. Euro"
         },
         total_cost_of_primary_nuclear: {
-          gquery: "total_cost_of_primary_nuclear",
+          gquery: "costs_share_of_uranium",
           unit: "Mln. Euro"
         }, 
         renewable: {
-          gquery: "total_cost_of_primary_renewable",
+          gquery: "costs_share_of_sustainable",
           unit: "Mln. Euro"
         }
       },
       dashboard: {
-        coal: "total_cost_of_primary_coal", 
-        natural_gas: "total_cost_of_primary_natural_gas", 
-        total_cost_of_primary_oil: "total_cost_of_primary_oil", 
-        total_cost_of_primary_nuclear: "total_cost_of_primary_nuclear", 
-        renewable: "total_cost_of_primary_renewable"
+        coal: "costs_share_of_coal", 
+        natural_gas: "costs_share_of_gas", 
+        total_cost_of_primary_oil: "costs_share_of_coal", 
+        total_cost_of_primary_nuclear: "costs_share_of_uranium", 
+        renewable: "costs_share_of_sustainable"
       }
     }
     
@@ -44,4 +44,5 @@ class PagesController < ApplicationController
       net_energy_import: "energy_dependence"
     }
   end
+
 end
