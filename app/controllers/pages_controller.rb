5c08e6c671a1ca3f4a717cbc323cf4c7b37410fc
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-03-16 12:05:02 +0100

    changed co2 query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -43,7 +43,7 @@ class PagesController < ApplicationController
     # This hash keys will be used to create the dashboard items. Mixer.js uses this keys too
     # to perform queries.
     @dashboard_items = [
-      { gquery: "co2_emission", label: "CO2 Emissions"},
+      { gquery: "co2_reduction_final_demand_to_1990_in_percent", label: "CO2 Emissions"},
       { gquery: "share_of_renewable_energy", label: "Share of renewable energy"},
       { gquery: "area_footprint_per_nl", label: "Area Footprint per NL"},
       { gquery: "energy_dependence", label: "Energy Dependence"}
