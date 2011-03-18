395a2f34b88010fe01b86a0674f746b00c03bafd
Paolo Zaccagnini <paozac@gmail.com>
2011-03-18 11:18:26 +0100

    cleanup pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_filter :find_scenario, :only => :scenario
+  before_filter :setup_results, :only => [:home, :save_cenario]
   skip_before_filter :authenticate_user!
 
   def home
@@ -14,47 +15,10 @@ class PagesController < ApplicationController
       :output_7 => 0,
       :output_8 => 0
     )
-    @questions = Question.ordered.all
-    @questions.each do |q|
+
+    Question.ordered.each do |q|
       @user_scenario.answers.build(:question_id => q.id)
     end
-    
-    # Be careful with this variable!
-    @results = {    
-      coal: {
-       gquery: "costs_share_of_coal",
-       unit: "Mln. Euro",
-       label: "Coal",
-       css_class: "coal"
-      },
-      gas: {
-       gquery: "costs_share_of_gas",
-       unit: "Mln. Euro",
-       label: "Gas",
-       css_class: "gas"
-      }, 
-      oil: {
-       gquery: "costs_share_of_oil",
-       unit: "Mln. Euro",
-       label: "Oil",
-       css_class: "oil"
-      },
-      nuclear: {
-       gquery: "costs_share_of_uranium",
-       unit: "Mln. Euro",
-       label: "Uranium",
-       css_class: "nuclear"
-       
-      }, 
-      renewable: {
-       gquery: "costs_share_of_sustainable",
-       unit: "Mln. Euro",
-       label: "Sustainable",
-       css_class: "renewable"       
-      }
-    }
-    
-    @dashboard_items = DashboardItem.ordered.all    
   end
   
   def scenario
@@ -65,7 +29,6 @@ class PagesController < ApplicationController
     if @user_scenario.save
       redirect_to scenario_path(@user_scenario), :notice => 'Scenario saved'
     else
-      @questions = Question.ordered.all
       render :home
     end
   end
@@ -76,5 +39,47 @@ class PagesController < ApplicationController
       @user_scenario = UserScenario.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       redirect_to root_path, :alert => "Scenario not found"
+    end
+    
+    # TODO: clean up
+    # We should move this parameters to a config file or db.
+    # I've moved it to a separate method because we need this
+    # variables if the scenario save fails and we have to show
+    # the page again
+    def setup_results
+      # Be careful with this variable!
+      @results = {    
+        coal: {
+         gquery: "costs_share_of_coal",
+         unit: "Mln. Euro",
+         label: "Coal",
+         css_class: "coal"
+        },
+        gas: {
+         gquery: "costs_share_of_gas",
+         unit: "Mln. Euro",
+         label: "Gas",
+         css_class: "gas"
+        }, 
+        oil: {
+         gquery: "costs_share_of_oil",
+         unit: "Mln. Euro",
+         label: "Oil",
+         css_class: "oil"
+        },
+        nuclear: {
+         gquery: "costs_share_of_uranium",
+         unit: "Mln. Euro",
+         label: "Uranium",
+         css_class: "nuclear"
+
+        }, 
+        renewable: {
+         gquery: "costs_share_of_sustainable",
+         unit: "Mln. Euro",
+         label: "Sustainable",
+         css_class: "renewable"       
+        }
+      }
     end
 end
