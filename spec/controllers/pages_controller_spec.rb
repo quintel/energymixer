2238c1049207696b788eefcc3d21fb14d720690c
Paolo Zaccagnini <paozac@gmail.com>
2011-03-21 14:48:23 +0100

    tests and hints to embed popups

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  let(:popup) { Factory :popup }
+  
+  describe "GET info" do
+    it "should show a popup page" do
+      get :info, :code => popup.code
+      response.should be_success
+    end
+    
+    it "should show a popup page via ajax" do
+      xhr :get, :info, :code => popup.code
+      response.should be_success
+    end
+  end
 end
