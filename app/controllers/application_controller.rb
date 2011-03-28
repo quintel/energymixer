2ae773f5610333035e9646cd8af4d5e1ace6f27b
Paolo Zaccagnini <paozac@gmail.com>
2011-03-28 16:58:27 +0200

    added I18n translation files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
   
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   protected
   
+    def set_locale
+      # if params[:locale] is nil then I18n.default_locale will be used
+      I18n.locale = params[:locale]
+    end
+
     def record_not_found
       flash[:alert] = 'Record not found'
       redirect_to :back
