ec38bde7db9b6a8a5d6accaad2a72aed11ad0e30
Paolo Zaccagnini <paozac@gmail.com>
2011-04-04 16:20:11 +0200

    fixed current scenario data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   def home
     # TODO: DRY, this same code appears on ScenarioController
     @current_scenario = Scenario.new(
-      :output_0  => 3948583406.649,
-      :output_1  => 19010942227.973732,
-      :output_2  => 14894948012.582876,
-      :output_3  => 2385593709.4479237,
-      :output_4  => 536620590.0778774,
-      :year => 2011
+      :output_0 => 3948583406.649,
+      :output_1 => 19010942227.973732,
+      :output_2 => 14894948012.582876,
+      :output_3 => 536620590.0778774,
+      :output_4 => 2385593709.4479237
+      :year     => 2011
     )    
   end
   
