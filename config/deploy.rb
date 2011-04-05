34cc56d97b5996d75b6426fd9a9db38ca250b18a
Paolo Zaccagnini <paozac@gmail.com>
2011-04-05 10:16:53 +0200

    fix hoptoad-capistrano issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,6 @@ ssh_options[:forward_agent] = true
 set :use_sudo,     false
 
 set :rvm_ruby_string, '1.9.2'
-#set :rvm_type, :user  # Don't use system-wide RVM
-        require 'config/boot'
-        require 'hoptoad_notifier/capistrano'
+
+require './config/boot'
+require 'hoptoad_notifier/capistrano'
