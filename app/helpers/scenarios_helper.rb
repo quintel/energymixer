0a8c622571857ca8c7190906ec2ad6991bfafa9a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-04-06 08:10:08 +0200

    changed layout and Lorem Ipsum text

--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -148,6 +148,6 @@ module ScenariosHelper
       "Terwijl onze eigen aardgasreserves krimpen gaan we vrolijk door met produceren en consumeren."
     end
     
-    out.map{|x| "<p>#{x}</p>"}.join.html_safe
+    out.map{|x| "<span>#{x}</span>"}.join.html_safe
   end
 end
