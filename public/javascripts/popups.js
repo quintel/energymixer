dbe10be104916c25c20d09e402a0079d122f899c
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-04-08 16:44:17 +0200

    removed console.log

--- public/javascripts/popups.js
+++ public/javascripts/popups.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
     var tipX = e.pageX - 0;
     var tipY = e.pageY - 0;
     var height = parseFloat($(this).parents(".charts_container").find(".renewables_float").css("height"));
-    console.log(height);
     $(".renewables_float").css({"top": tipY - height/2 , "left": tipX + 15});
   });
 });
