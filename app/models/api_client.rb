f273f5ac23f670e4c5aa680bfaa5de545a90c7ef
Paolo Zaccagnini <paozac@gmail.com>
2011-04-11 11:13:27 +0200

    homepage animation

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -16,6 +16,40 @@ class ApiClient
         "costs_share_of_sustainable_solar",
         "costs_share_of_sustainable_biomass"
       ]
+      
+  CarrierCostsGQueries = [
+    "costs_share_of_coal",
+    "costs_share_of_gas",
+    "costs_share_of_oil",
+    "costs_share_of_uranium",
+    "costs_share_of_sustainable",
+
+    "costs_share_of_oil_buildings",
+    "costs_share_of_gas_buildings",
+    "costs_share_of_coal_buildings",
+    "costs_share_of_uranium_buildings",
+    "costs_share_of_sustainable_buildings",
+
+    "costs_share_of_oil_industry",
+    "costs_share_of_gas_industry",
+    "costs_share_of_coal_industry",
+    "costs_share_of_uranium_industry",
+    "costs_share_of_sustainable_industry",
+
+    "costs_share_of_oil_transport",
+    "costs_share_of_gas_transport",
+    "costs_share_of_coal_transport",
+    "costs_share_of_uranium_transport",
+    "costs_share_of_sustainable_transport",
+
+    "costs_share_of_oil_agriculture",
+    "costs_share_of_gas_agriculture",
+    "costs_share_of_coal_agriculture",
+    "costs_share_of_uranium_agriculture",
+    "costs_share_of_sustainable_agriculture"
+  ]
+  
+  # TODO: DRY with caching, handle timeout and other errors
 
   def current_situation
     Rails.cache.fetch("current_situation") do
@@ -23,26 +57,42 @@ class ApiClient
     end
   end
     
+  def current_situation!
+    query(ApiClient::GQueries)
+  end
+
+  def carrier_costs
+    Rails.cache.fetch("carrier_costs") do
+      carrier_costs!
+    end
+  end
+    
+  def carrier_costs!
+    query(ApiClient::CarrierCostsGQueries)
+  end
+
   def api_session_key
     @api_session_key ||= api_session_key!
   end
   
-  def current_situation!
-    url   = "/api/v1/api_scenarios/#{api_session_key}.json"
-    query = { result: ApiClient::GQueries, reset: 1 }
-    
-    response = self.class.get(url, :query => query)
-    out = {}
-    response["result"].each_pair{|k,v| out[k] = v[0][1]}
-    out
-  rescue
-    nil
-  end
-
   def api_session_key!
     response = self.class.get("/api/v1/api_scenarios/new.json")
     response["api_scenario"]["api_session_key"]
   rescue
     nil
-  end  
+  end
+  
+  private
+  
+    def query(gqueries)
+        url   = "/api/v1/api_scenarios/#{api_session_key}.json"
+        query = { result: gqueries, reset: 1 }
+
+        response = self.class.get(url, :query => query)
+        out = {}
+        response["result"].each_pair{|k,v| out[k] = v[0][1]}
+        out
+      rescue
+        nil
+    end
 end
--- public/javascripts/home.animation.js
+++ public/javascripts/home.animation.js
@@ -1,53 +1,66 @@
-$(document).ready(function(){
+function Home() {
+  // Prevent calling the function without the new operator
+  if ( !(this instanceof arguments.callee) ) {
+    return new arguments.callee(arguments);
+  }
+
+  var self = this;
   
-  var shares = {
-    industry: {
-      coal: .50,
-      gas: .50,
-      oil: .50,
-      nuclear: .50,
-      renewable: .50,
-      money: .50
-    },
-    transport: {
-      coal: .15,
-      gas: .10,
-      oil: .20,
-      nuclear: .50,
-      renewable: .5
-    },
-    buildings: {
-      coal: .15,
-      gas: .10,
-      oil: .20,
-      nuclear: .50,
-      renewable: .5
-    },
-    agriculture: {
-      coal: .15,
-      gas: .10,
-      oil: .20,
-      nuclear: .50,
-      renewable: .5
-    }
+  self.shares = {};
+  
+  // show cumulative data
+  self.reset_carriers = function() {
+    self.update_carriers('total', 1000);
   };
   
-  function update_carriers (carrier_values) {
+  self.update_carriers = function(key, speed) {
+    if(typeof(speed) == "undefined") { speed = 350 }
+    self._update_carriers(self.shares[key], speed);
+  };
+  
+  self._update_carriers = function (carrier_values, speed) {
     $.each(carrier_values, function(carrier, value){
-      var carrier_li = $("ul.chart").find("."+carrier);
-      var current_height = carrier_li.height();
-      carrier_li.animate({"height": current_height * value}, 2000);
+      
+      var original_height = self.original_height[carrier];
+      var new_height = value / self.shares["total"][carrier] * original_height;
+      
+      var carrier_li = $("ul.chart").find("." + carrier);
+      carrier_li.stop(true);
+      carrier_li.animate({"height": new_height}, speed);
+      var label = carrier_li.find(".label");
+      new_height > 11 ? label.show() : label.hide();
     });
   };
   
-  update_carriers(shares.industry);
-    
-  // var new_height = Math.round(val / current_sum * current_graph_height);
-  // rounded_sum += new_height;
-  // var selector = ".user_created ." + code;
-  // $(selector).animate({"height": new_height}, "slow");
-  // // hide text if there's no room
-  // var label = $(selector + " .label");
-  // new_height > 10 ? label.show() : label.hide();  
+  self._save_original_height = function() {
+    self.original_height = {
+      coal:      $("ul.chart li.coal").height(),
+      gas:       $("ul.chart li.gas").height(),
+      oil:       $("ul.chart li.oil").height(),
+      nuclear:   $("ul.chart li.nuclear").height(),
+      renewable: $("ul.chart li.renewable").height()
+    }
+  };
   
+  self.init = function() {
+    self.shares = globals.carriers;
+    self._save_original_height();
+  };
+  
+  self.init();
+}
+
+$(document).ready(function(){
+  h = new Home();
+  
+  $("#sectors .sector").hover(
+    // handlerin
+    function(){
+      var sector_id = $(this).attr("id");
+      h.update_carriers(sector_id);
+    },
+    // handlerout
+    function(){
+      h.reset_carriers();
+    });
 });
