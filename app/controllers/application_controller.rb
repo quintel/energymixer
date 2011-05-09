a36c1308f002babd526ab06eb386667341dfc2d0
robbertdol <robbertdol@quintel.com>
2011-05-09 19:36:33 +0200

    added the option to make the text unselectlectable for the touchscreen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
+  before_filter :set_locale,:check_touchsceen
   
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   protected
   
+    ##
+    # When the et-model is in touchscreen mode the text must be unselectable
+    def check_touchsceen
+      if params[:touchscreen] == "true"
+        session[:touchscreen] = true
+      elsif params[:touchscreen] == "reset"      
+        session[:touchscreen] = nil
+      end
+    end
+    
     def set_locale
       # if params[:locale] is nil then I18n.default_locale will be used
       I18n.locale = params[:locale]
