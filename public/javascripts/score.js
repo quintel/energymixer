c46d0437781b775e5bd022a077867b9b864b8e0f
Paolo Zaccagnini <paozac@gmail.com>
2011-06-14 16:49:14 +0200

    update score calculation

--- public/javascripts/score.js
+++ public/javascripts/score.js
@@ -21,25 +21,25 @@ function Score() {
     }
     
     var v = self.values.total_amount;
-    var a = (v.mark - v.current) / v.mark * 100;
+    var a = (v.mark - v.current) / 1000;
     if (a < 0) { a = 0; }
     
     v = self.values.co2_emission_percent_change_from_1990_corrected_for_electricity_import;
     var b = 0;
     if (v.mark > v.current) {
-      b = Math.abs((v.mark - v.current) / v.mark * 100);
+      b = Math.abs((v.mark - v.current) * 100);
     }
     
     v = self.values.share_of_renewable_energy;
-    var c = (v.current - v.mark) / v.mark * 100;
+    var c = (v.current - v.mark) * 100;
     if (c < 0) { c = 0; }    
     
     v = self.values.area_footprint_per_nl;
-    var d = (v.mark - v.current) / v.mark * 100;
+    var d = (v.mark - v.current) * 100;
     if (d < 0) { d = 0; }
     
     v = self.values.energy_dependence;
-    var e = (v.mark - v.current) / v.mark * 100;
+    var e = (v.mark - v.current) * 100;
     if (e < 0) { e = 0; }
 
     return a + b + c + d + e;
