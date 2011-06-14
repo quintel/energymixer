8d5dcd4627c8406be76b807ce850833215a1d7b6
Paolo Zaccagnini <paozac@gmail.com>
2011-06-14 11:54:48 +0200

    capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,48 @@
 require 'bundler/capistrano'
 
 set :application, "energymixer"
-
-set :stage, :production
-
-set :domain, "79.125.109.178"
-role :web, domain # Your HTTP server, Apache/etc
-role :app, domain # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
-
-set :user, 'ubuntu'
-
-
-set :deploy_to, "/home/ubuntu/apps/#{application}"
-set :config_files, "/home/ubuntu/config_files"
-
 set :scm, :git
 set :repository,  "git@github.com:dennisschoenmakers/energymixer.git"
-set :scm, "git"
+
+set :user, 'ubuntu'
+set :deploy_to, "/home/ubuntu/apps/#{application}"
+set :config_files, "/home/ubuntu/config_files"
 set :deploy_via, :remote_cache
-set :chmod755, "app config db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions set :use_sudo, false
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :git_enable_submodules, 1
-set :branch, "master"
 ssh_options[:forward_agent] = true
-
-
-set :use_sudo,     false
-
+set :use_sudo, false
 set :rvm_ruby_string, '1.9.2'
+
+task :to_shell do
+  set :domain, "79.125.109.178"
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
+  set :branch, "master"
+end
+
+task :to_gasmixer do
+  set :domain, "46.137.111.149"
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
+  set :branch, "master"
+end
+
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  task :create_symlinks do
+    run "cp #{config_files}/database.yml #{release_path}/config/database.yml"
+    run "cp #{config_files}/config.yml #{release_path}/config/config.yml"
+  end
+end
+
+after 'deploy:update_code', 'deploy:create_symlinks'
 
 require './config/boot'
 require 'hoptoad_notifier/capistrano'
