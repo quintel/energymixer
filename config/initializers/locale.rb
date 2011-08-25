4b28e0e7af6b28928c2ca4ab757b26fb2a8658f0
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-08-25 14:40:43 +0200

    added discovery localles and added title as first translation

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,5 @@
+#Load subdirectories in config/locales
+config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
+
 # Use AR store if key is available, otherwise fallback on YAML
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n.backend)
