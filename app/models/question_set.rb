04593e44b1e4f713f342dd5c8eba907831a6f3c5
Paolo Zaccagnini <paozac@gmail.com>
2011-08-30 11:48:09 +0200

    question sets: calculate all possible combinations

--- app/models/question_set.rb
+++ app/models/question_set.rb
@@ -4,25 +4,25 @@ class QuestionSet < ActiveRecord::Base
   has_many :questions
   
   def combinations
-    qs = {}.tap{|x| questions.ordered.each{|q| x[q.id] = q.answer_ids + [nil] }}
-    answers = qs.values.take(2)
-    cartesian(*answers)
+    answers = questions.ordered.map{|q| q.answer_ids + [nil]}
+    answers[0].compact!
+    answers[0].product(*answers[1..-1]).select{|x| valid_combination?(x)}
   end
 
   private
-
-    def cartesian(*args)
-      final_output= [[]]
-      while [] != args
-        t, final_output= final_output, []
-        b, *args = args
-        t.each do |a|
-          b.each do |n|
-            next if n.nil?
-            final_output<< a + [n]
-          end
+  
+    def valid_combination?(x)
+      nil_found = false
+      for i in 0..(x.size)
+        if x[i].nil?
+          nil_found = true 
         end
+        # if there's a nil in the middle I won't accept following not-nil values
+        # 1/2/3/nil => OK
+        # 1/nil => OK
+        # 1/nil/2 => NO
+        return false if nil_found && x[i]
       end
-      final_output
+      return true
     end
 end
