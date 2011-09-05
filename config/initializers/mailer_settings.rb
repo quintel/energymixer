dd597975429e0d0c066400b29affaa22b5c48f46
Paolo Zaccagnini <paozac@gmail.com>
2011-09-05 14:09:55 +0200

    fixed mailer deprecation warning

--- config/initializers/mailer_settings.rb
+++ config/initializers/mailer_settings.rb
@@ -8,5 +8,5 @@ end
 
 # TODO: update as needed
 if APP_CONFIG['app_name']
-  ActionMailer::Base.template_root = "app/views/" + APP_CONFIG["app_name"]
+  ActionMailer::Base.prepend_view_path("app/views/" + APP_CONFIG["app_name"])
 end
