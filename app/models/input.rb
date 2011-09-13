19044e22a568edb88ddf3cb53bde0c7333650664
Paolo Zaccagnini <paozac@gmail.com>
2011-09-13 11:36:03 +0200

    some refactoring following sebi's suggestions

--- app/models/input.rb
+++ app/models/input.rb
@@ -29,11 +29,15 @@ class Input < ActiveRecord::Base
   def self.available_inputs
     Rails.cache.fetch('available_inputs') do
       hash = {}
-      Api::Input.all.sort{|a,b| a.key <=> b.key }.each{|i| hash[i.id] = i.key}
+      Api::Input.all.sort_by(&:key).each{|i| hash[i.id] = i.key}
       hash
       # DEBT: one-liner
       # Api::Input.all.sort_by(:key).inject({}) {|hsh,i| hsh.merge i.id => i.key}
+      # +each+ makes the code easier to read and is *way* faster
+      #       user     system      total        real
+      # each:  1.040000   0.000000   1.040000 (  1.035372)
+      # inject: 18.190000   0.080000  18.270000 ( 18.232147)
+      # - PZ
     end
-  end    
+  end
 end
-
