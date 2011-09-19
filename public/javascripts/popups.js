09f3e5e658f478a4ad32db32c78964f444caf25b
Paolo Zaccagnini <paozac@gmail.com>
2011-09-19 16:05:03 +0200

    renewables popup

--- public/javascripts/popups.js
+++ public/javascripts/popups.js
@@ -1,17 +1,13 @@
-$(document).ready(function(){
-  // hover on renewables
-  $(".chart .renewable").hover(
-    function(){
-      $(this).parents(".charts_container").find(".renewables_float").show();
-    },
-    function(){
-      $(".renewables_float").hide();
-    }
-  ).mousemove(function(e){
-    var tipX = e.pageX - 0;
-    var tipY = e.pageY - 0;
-    var height = parseFloat($(this).parents(".charts_container").find(".renewables_float").css("height"));
-    $(".renewables_float").css({"top": tipY - height/2 , "left": tipX + 15});
+/* DO NOT MODIFY. This file was compiled Mon, 19 Sep 2011 14:01:44 GMT from
+ * /Users/paozac/Sites/energymixer/app/coffeescripts/popups.coffee
+ */
+
+(function() {
+  $(function() {
+    return $(".chart .renewable").hover(function() {
+      return $(this).parents(".charts_container").find(".renewables_float").show();
+    }, function() {
+      return $(".renewables_float").hide();
+    });
   });
-  
-});
+}).call(this);
