ca4606ed54801fccfcf8203925620cfa30afb746
Paolo Zaccagnini <paozac@gmail.com>
2011-10-10 17:07:09 +0200

    adapting backbone

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,95 +1,98 @@
-/* DO NOT MODIFY. This file was compiled Mon, 10 Oct 2011 09:45:11 GMT from
+/* DO NOT MODIFY. This file was compiled Tue, 11 Oct 2011 10:16:18 GMT from
  * /Users/paozac/Sites/energymixer/app/coffeescripts/home.coffee
  */
 
 (function() {
-  var Home;
-  var __hasProp = Object.prototype.hasOwnProperty, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var Home, HomeView;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
   Home = (function() {
+    __extends(Home, Backbone.Model);
     function Home() {
-      this.current_sector = false;
-      this.max_height = 360;
-      this.shares = globals.carriers;
-      this.setup_callbacks();
+      this.price = __bind(this.price, this);
+      Home.__super__.constructor.apply(this, arguments);
     }
-    Home.prototype._update_price = function() {
-      var amount, formatted_amount;
-      if (this.current_sector) {
-        amount = this.shares.sectors[this.current_sector].total * this.shares.total.amount;
+    Home.prototype.initialize = function() {
+      return this.shares = window.globals.carriers;
+    };
+    Home.prototype.price = function() {
+      var amount, current_sector;
+      current_sector = this.get("current_sector");
+      if (current_sector) {
+        amount = this.shares.sectors[current_sector].total * this.shares.total.amount;
       } else {
         amount = this.shares.total.amount;
       }
-      formatted_amount = sprintf("%.1f", amount / 1000000000);
-      return $("#chart header span.amount").html(formatted_amount);
-    };
-    Home.prototype._update_chart = function() {
-      var carrier, carrier_li, new_height, ratio, ratios, _results;
-      if (this.current_sector) {
-        ratios = this.shares.sectors[this.current_sector].carriers;
-      } else {
-        ratios = this.shares.total;
-      }
-      _results = [];
-      for (carrier in ratios) {
-        if (!__hasProp.call(ratios, carrier)) continue;
-        ratio = ratios[carrier];
-        new_height = this.max_height * ratio;
-        carrier_li = $("ul.chart").find("." + carrier);
-        _results.push(carrier_li.stop(true).animate({
-          "height": new_height
-        }, 500));
-      }
-      return _results;
-    };
-    Home.prototype._update_percentages = function() {
-      var carrier, ratio, ratios, value, _results;
-      if (this.current_sector) {
-        ratios = this.shares.sectors[this.current_sector].carriers;
-      } else {
-        ratios = this.shares.total;
-      }
-      _results = [];
-      for (carrier in ratios) {
-        if (!__hasProp.call(ratios, carrier)) continue;
-        ratio = ratios[carrier];
-        value = Math.round(ratio * 100);
-        _results.push($("#chart tr." + carrier + " .value").html("" + value + "%"));
-      }
-      return _results;
-    };
-    Home.prototype.reset_map = function() {
-      this.current_sector = false;
-      return this.update_map();
-    };
-    Home.prototype.update_map = function() {
-      this._update_price();
-      this._update_chart();
-      return this._update_percentages();
-    };
-    Home.prototype.setup_callbacks = function() {
-      $("#sector_links .sector a, #sector_icons .sector").hover(__bind(function(e) {
-        var popup, sector_id;
-        sector_id = $(e.target).attr("rel");
-        popup = $("#sector_icons .sector[rel=" + sector_id + "] .popup");
-        popup.show();
-        this.current_sector = sector_id;
-        return this.update_map();
-      }, this), __bind(function(e) {
-        $(".popup").hide();
-        return this.reset_map();
-      }, this));
-      return $("#sector_icons a").click(__bind(function(e) {
-        var container;
-        e.preventDefault();
-        container = $(e.target).parent();
-        container.find(".text").toggle();
-        return $(e.target).toggleClass("close_description");
-      }, this));
+      return sprintf("%.1f", amount / 1000000000);
     };
     return Home;
   })();
+  HomeView = (function() {
+    __extends(HomeView, Backbone.View);
+    function HomeView() {
+      this.update_current_sector = __bind(this.update_current_sector, this);
+      this.reset_current_sector = __bind(this.reset_current_sector, this);
+      this.show_information = __bind(this.show_information, this);
+      this.render = __bind(this.render, this);
+      HomeView.__super__.constructor.apply(this, arguments);
+    }
+    HomeView.prototype.initialize = function() {
+      this.current_sector = false;
+      this.max_height = 360;
+      this.model = new Home();
+      this.model.bind("change:current_sector", this.render);
+      return this.render();
+    };
+    HomeView.prototype.model = Home;
+    HomeView.prototype.el = "#map";
+    HomeView.prototype.events = {
+      "click #sector_icons a.description": "show_information",
+      "mouseover #sector_links .sector a": "update_current_sector",
+      "mouseover #sector_icons .sector": "update_current_sector",
+      "mouseout #sector_links .sector a": "reset_current_sector",
+      "mouseout #sector_icons .sector": "reset_current_sector"
+    };
+    HomeView.prototype.render = function() {
+      var popup, sector_id;
+      sector_id = this.model.get("current_sector");
+      if (sector_id) {
+        popup = $("#sector_icons .sector[rel=" + sector_id + "] .popup");
+        popup.show();
+      } else {
+        $(".popup").hide();
+      }
+      $("#chart header span.amount").html(this.model.price());
+      return this.el;
+    };
+    HomeView.prototype.show_information = function(e) {
+      var container;
+      console.log("Yo");
+      e.preventDefault();
+      container = $(e.target).parent();
+      container.find(".text").toggle();
+      return $(e.target).toggleClass("close_description");
+    };
+    HomeView.prototype.reset_current_sector = function() {
+      return this.model.set({
+        'current_sector': false
+      });
+    };
+    HomeView.prototype.update_current_sector = function(e) {
+      var sector_id;
+      sector_id = $(e.target).attr("rel");
+      return this.model.set({
+        current_sector: sector_id
+      });
+    };
+    return HomeView;
+  })();
   $(function() {
-    var h;
-    return h = new Home;
+    return window.h = new HomeView();
   });
 }).call(this);
