166ea088428c580ae9bb33d479fc82ed6d383b98
Paolo Zaccagnini <paozac@gmail.com>
2011-10-11 12:44:25 +0200

    update chart

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Tue, 11 Oct 2011 10:16:18 GMT from
+/* DO NOT MODIFY. This file was compiled Tue, 11 Oct 2011 11:47:01 GMT from
  * /Users/paozac/Sites/energymixer/app/coffeescripts/home.coffee
  */
 
@@ -38,12 +38,12 @@
     function HomeView() {
       this.update_current_sector = __bind(this.update_current_sector, this);
       this.reset_current_sector = __bind(this.reset_current_sector, this);
+      this.update_chart = __bind(this.update_chart, this);
       this.show_information = __bind(this.show_information, this);
       this.render = __bind(this.render, this);
       HomeView.__super__.constructor.apply(this, arguments);
     }
     HomeView.prototype.initialize = function() {
-      this.current_sector = false;
       this.max_height = 360;
       this.model = new Home();
       this.model.bind("change:current_sector", this.render);
@@ -53,21 +53,22 @@
     HomeView.prototype.el = "#map";
     HomeView.prototype.events = {
       "click #sector_icons a.description": "show_information",
-      "mouseover #sector_links .sector a": "update_current_sector",
-      "mouseover #sector_icons .sector": "update_current_sector",
-      "mouseout #sector_links .sector a": "reset_current_sector",
-      "mouseout #sector_icons .sector": "reset_current_sector"
+      "mouseenter #sector_links .sector a": "update_current_sector",
+      "mouseenter #sector_icons .sector": "update_current_sector",
+      "mouseleave #sector_links .sector a": "reset_current_sector",
+      "mouseleave #sector_icons .sector": "reset_current_sector"
     };
     HomeView.prototype.render = function() {
-      var popup, sector_id;
-      sector_id = this.model.get("current_sector");
-      if (sector_id) {
-        popup = $("#sector_icons .sector[rel=" + sector_id + "] .popup");
+      var popup;
+      this.sector_id = this.model.get("current_sector");
+      if (this.sector_id) {
+        popup = $("#sector_icons .sector[rel=" + this.sector_id + "] .popup");
         popup.show();
       } else {
         $(".popup").hide();
       }
       $("#chart header span.amount").html(this.model.price());
+      this.update_chart();
       return this.el;
     };
     HomeView.prototype.show_information = function(e) {
@@ -77,6 +78,28 @@
       container = $(e.target).parent();
       container.find(".text").toggle();
       return $(e.target).toggleClass("close_description");
+    };
+    HomeView.prototype.update_chart = function() {
+      var carrier, carrier_li, new_height, ratio, ratios, shares, value, _results;
+      shares = this.model.shares;
+      if (this.sector_id) {
+        ratios = shares.sectors[this.sector_id].carriers;
+      } else {
+        ratios = shares.total;
+      }
+      _results = [];
+      for (carrier in ratios) {
+        if (!__hasProp.call(ratios, carrier)) continue;
+        ratio = ratios[carrier];
+        new_height = this.max_height * ratio;
+        carrier_li = $("ul.chart").find("." + carrier);
+        carrier_li.stop(true).animate({
+          "height": new_height
+        }, 500);
+        value = Math.round(ratio * 100);
+        _results.push($("#chart tr." + carrier + " .value").html("" + value + "%"));
+      }
+      return _results;
     };
     HomeView.prototype.reset_current_sector = function() {
       return this.model.set({
