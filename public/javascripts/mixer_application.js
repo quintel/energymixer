413845583f96cd27d47e3805dbd16910006bae32
Paolo Zaccagnini <paozac@gmail.com>
2011-10-11 17:37:21 +0200

    app is now a backbone model

--- public/javascripts/mixer_application.js
+++ public/javascripts/mixer_application.js
@@ -1,15 +1,28 @@
-/* DO NOT MODIFY. This file was compiled Wed, 14 Sep 2011 08:25:43 GMT from
+/* DO NOT MODIFY. This file was compiled Tue, 11 Oct 2011 15:36:56 GMT from
  * /Users/paozac/Sites/energymixer/app/coffeescripts/mixer_application.coffee
  */
 
 (function() {
-  this.App = (function() {
+  var App;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
+  App = (function() {
+    __extends(App, Backbone.Model);
     function App() {
+      App.__super__.constructor.apply(this, arguments);
+    }
+    App.prototype.initialize = function() {
       this.mixer = new Mixer(this);
       this.chart = new Chart(this);
       this.questions = new Questions(this);
-      this.score = new Score(this);
-    }
+      return this.score = new Score(this);
+    };
     return App;
   })();
   $(function() {
