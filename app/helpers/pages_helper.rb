1f3b2457c7d39c4011016dc677f4f3a9d243bdd1
Paolo Zaccagnini <paozac@gmail.com>
2011-10-31 11:21:06 +0100

    embed_video helper and forms to update the field

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,11 @@ module PagesHelper
   def markdown(s)
     BlueCloth.new(s).to_html.html_safe rescue nil
   end
+  
+  def embed_video(popup)
+    return unless url = popup.video
+    if url =~ /youtube/
+      %(<object width="640" height="385"><param name="movie" value="#{url}"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="#{url}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="300"></embed></object>).html_safe
+    end
+  end
 end
