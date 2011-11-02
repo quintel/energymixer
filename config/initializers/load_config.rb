32d2d48f02086e83a80f2ad131276b4ffcab9175
Paolo Zaccagnini <paozac@gmail.com>
2011-11-02 12:28:05 +0100

    made APP_CONFIG a hash with indifferent access and removed unused END_YEAR constant

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,2 @@
-APP_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env]
-APP_NAME = APP_CONFIG['app_name']
-END_YEAR = APP_CONFIG['api_session_settings'][:end_year]
+APP_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env].with_indifferent_access
+APP_NAME = APP_CONFIG[:app_name]
