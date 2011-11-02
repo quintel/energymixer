89d1d78e8227ac715b0a49c729d6e0b2ff20dd70
Paolo Zaccagnini <paozac@gmail.com>
2011-11-02 12:29:10 +0100

    hide youtube videos on standalone server

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,7 @@ module PagesHelper
   
   def embed_video(popup)
     return unless url = popup.video
-    if url =~ /youtube/
+    if url =~ /youtube/ && !APP_CONFIG[:standalone]
       %(<object width="640" height="385"><param name="movie" value="#{url}"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="#{url}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="300"></embed></object>).html_safe
     end
   end
