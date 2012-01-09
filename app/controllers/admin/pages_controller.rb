03a5e5ea36997556aa618e8d0bc5c59eb0bac1c9
Paolo Zaccagnini <paozac@gmail.com>
2012-01-09 15:48:47 +0100

    dropped API back-end selector
    the feature will be reintroduced later

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,9 +1,5 @@
 class Admin::PagesController < AdminController
-  skip_before_filter :set_api_base_path, :only => :index
-  
   def index
-    session[:api_base_path] = params[:api_base_path] unless params[:api_base_path].blank?
-    set_api_base_path
   end
   
   def reset_cache
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale, :check_touchsceen, :set_api_base_path
+  before_filter :set_locale, :check_touchsceen
 
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   protected
 
-    ##
-    # When the et-model is in touchscreen mode the text must be unselectable
-    def check_touchsceen
-      if params[:touchscreen] == "true"
-        session[:touchscreen] = true
-      elsif params[:touchscreen] == "reset"
-        session[:touchscreen] = nil
-      end
+  ##
+  # When the et-model is in touchscreen mode the text must be unselectable
+  def check_touchsceen
+    if params[:touchscreen] == "true"
+      session[:touchscreen] = true
+    elsif params[:touchscreen] == "reset"
+      session[:touchscreen] = nil
     end
+  end
 
-    def set_locale
-      session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
-      session[:locale] = :nl unless [:nl, :de].include?(session[:locale].to_sym)
-      I18n.locale = session[:locale]
-    end
+  def set_locale
+    session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
+    session[:locale] = :nl unless [:nl, :de].include?(session[:locale].to_sym)
+    I18n.locale = session[:locale]
+  end
 
-    def record_not_found
-      flash[:alert] = 'Record not found'
-      redirect_to :back
-    rescue
-      redirect_to root_path
-    end
+  def record_not_found
+    flash[:alert] = 'Record not found'
+    redirect_to :back
+  rescue
+    redirect_to root_path
+  end
 
-    def load_question_set
-      @question_set = QuestionSet.find_by_name(APP_NAME) || QuestionSet.first
-      @end_year = @question_set.try(:end_year)
-    end
-
-    def set_api_base_path
-      @api_base_path = session[:api_base_path] || APP_CONFIG["api_base_path"]
-    end
+  def load_question_set
+    @question_set = QuestionSet.find_by_name(APP_NAME) || QuestionSet.first
+    @end_year = @question_set.try(:end_year)
+  end
 end
