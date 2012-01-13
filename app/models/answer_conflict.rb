19dafb2d0434ee484afefddf02f52e9e9bd11e46
Paolo Zaccagnini <paozac@gmail.com>
2012-01-13 10:50:09 +0100

    whitespace

--- app/models/answer_conflict.rb
+++ app/models/answer_conflict.rb
@@ -10,27 +10,27 @@
 class AnswerConflict < ActiveRecord::Base
   belongs_to :answer
   belongs_to :other_answer, :class_name => 'Answer'
-  
+
   validates :answer_id,       :presence   => true
   validates :other_answer_id, :presence   => true
   validates :answer_id,       :uniqueness => { :scope => :other_answer_id }
   validates :other_answer_id, :uniqueness => { :scope => :answer_id }
   validate :no_duplicates
   validate :no_conflicts_with_the_same_question
-  
+
   attr_accessible :answer_id, :other_answer_id
-  
+
   private
-  
-    def no_duplicates
-      if AnswerConflict.exists?(:answer_id => answer_id, :other_answer_id => other_answer_id) || 
-         AnswerConflict.exists?(:other_answer_id => answer_id, :answer_id => other_answer_id)
-          errors.add(:base, "Conflict already defined")
-      end
+
+  def no_duplicates
+    if AnswerConflict.exists?(:answer_id => answer_id, :other_answer_id => other_answer_id) ||
+      AnswerConflict.exists?(:other_answer_id => answer_id, :answer_id => other_answer_id)
+      errors.add(:base, "Conflict already defined")
     end
-    
-    def no_conflicts_with_the_same_question
-      return unless answer && other_answer
-      errors.add(:base, "Conflict within the same question") if answer.question_id == other_answer.question_id
-    end
+  end
+
+  def no_conflicts_with_the_same_question
+    return unless answer && other_answer
+    errors.add(:base, "Conflict within the same question") if answer.question_id == other_answer.question_id
+  end
 end
--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -35,11 +35,11 @@
 class Scenario < ActiveRecord::Base
   MaxAmount = APP_CONFIG["max_total_cost"] || 100_000_000_000
   CurrentTitle = 'Current Scenario'
-  
+
   has_many :answers, :class_name => 'ScenarioAnswer', :dependent => :destroy
 
   accepts_nested_attributes_for :answers
-  
+
   # Be careful, these values must match the dashboard items
   # We could store this mapping in the db, but let's keep things simple
   Outputs = {
@@ -57,7 +57,7 @@ class Scenario < ActiveRecord::Base
     output_11: "share_of_total_costs_assigned_to_biomass",
     output_12: "mixer_total_costs"
   }
-  
+
   PrimaryTable = {
     coal:       "share_of_total_costs_assigned_to_coal",
     gas:        "share_of_total_costs_assigned_to_gas",
@@ -65,13 +65,13 @@ class Scenario < ActiveRecord::Base
     nuclear:    "share_of_total_costs_assigned_to_nuclear",
     renewables: "share_of_total_costs_assigned_to_renewables"
   }
-  
+
   SecondaryTable = {
     wind:    "share_of_total_costs_assigned_to_wind",
     solar:   "share_of_total_costs_assigned_to_solar",
     biomass: "share_of_total_costs_assigned_to_biomass"
   }
-  
+
   # The keys are used in the translation files
   DashboardTable = {
     co2_emissions: "mixer_reduction_of_co2_emissions_versus_1990",
@@ -79,19 +79,19 @@ class Scenario < ActiveRecord::Base
     footprint:     "mixer_bio_footprint",
     energy_import: "mixer_net_energy_import"
   }
-  
+
   CostsTable = {
     total_costs: "mixer_total_costs"
   }
-  
+
   validates :name, :presence => true
   # validates :email, :presence => true
   # disabled, client_side_validations has some issues with this validation
   # validates :age,   :numericality => true, :allow_blank => true
-  
+
   Outputs.keys.each {|key| validates key, :presence => true }
   validates :accept_terms, :acceptance => true
-  
+
   scope :recent_first, order('created_at DESC')
   scope :public,       where(:public => true)
   scope :featured,     where(:featured => true)
@@ -101,12 +101,12 @@ class Scenario < ActiveRecord::Base
   scope :featured_first, order('featured DESC')
   scope :by_user, lambda {|q| where('name LIKE ?', "%#{q}%") unless q.blank? }
   scope :excluding, lambda{|s| where('id != ?', s)}
-  
+
   attr_accessor :accept_terms
   before_save :sanitize_age
-  
+
   paginates_per 10
-    
+
   def carrier_ratios
     {
       coal: output_0,
@@ -124,23 +124,23 @@ class Scenario < ActiveRecord::Base
       biomass: output_11
     }
   end
-    
+
   def combined_carriers
     carrier_ratios.merge(renewable_carrier_ratios)
   end
-    
+
   def total_amount
     val = output_12 || (output_0 + output_1 + output_2 + output_3 + output_4)
     # Old scenarios used billions as unit
     val *= 1_000_000_000 if val < 1_000_000_000
     val
   end
-  
+
   # forces reload
   def self.current!
     current(true)
   end
-  
+
   # store in cache when needed
   def self.current(force = false)
     Rails.cache.delete('current_scenario') if force
@@ -153,35 +153,35 @@ class Scenario < ActiveRecord::Base
   rescue # some acceptable values, should the api request fail
     @current_scenario = self.acceptable_scenario
   end
-  
+
   def sanitize_age
     self.age = nil if self.age.to_i == 0
   end
-  
+
   # Returns plausible scenario if for any reason the API request fails
   def self.acceptable_scenario
     new(
-      :output_0  => 4135606319.8158274,    # coal
-      :output_1  => 19712261358.58237,     # gas
-      :output_2  => 15600440661.944386,    # oil
-      :output_3  => 562037387.8724155,     # nuclear
-      :output_4  => 2695124317.5551677,    # renewable      
-      :output_5  => -0.005590499343000932, # mixer_co2_reduction_from_1990
-      :output_6  => 0.05393912138004123,   # mixer_renewability
-      :output_7  => 0.6045053247669468,    # mixer_bio_footprint
-      :output_8  => 0.2477505720053616,    # mixer_net_energy_import
-      :output_9  => 212589519.75315946,    # share_of_total_costs_assigned_to_renewables_wind
-      :output_10 => 2141265.8747384516,    # share_of_total_costs_assigned_to_renewables_solar
-      :output_11 => 2170862923.8200254,    # share_of_total_costs_assigned_to_renewables_biomass
-      :output_12 => 42.420507732497605,    # mixer_total_costs
-      :year      => 2011
+    :output_0  => 4135606319.8158274,    # coal
+    :output_1  => 19712261358.58237,     # gas
+    :output_2  => 15600440661.944386,    # oil
+    :output_3  => 562037387.8724155,     # nuclear
+    :output_4  => 2695124317.5551677,    # renewable
+    :output_5  => -0.005590499343000932, # mixer_co2_reduction_from_1990
+    :output_6  => 0.05393912138004123,   # mixer_renewability
+    :output_7  => 0.6045053247669468,    # mixer_bio_footprint
+    :output_8  => 0.2477505720053616,    # mixer_net_energy_import
+    :output_9  => 212589519.75315946,    # share_of_total_costs_assigned_to_renewables_wind
+    :output_10 => 2141265.8747384516,    # share_of_total_costs_assigned_to_renewables_solar
+    :output_11 => 2170862923.8200254,    # share_of_total_costs_assigned_to_renewables_biomass
+    :output_12 => 42.420507732497605,    # mixer_total_costs
+    :year      => 2011
     )
   end
-  
+
   def current_scenario?
     self.featured? && self.title == CurrentTitle
   end
-  
+
   def has_coordinates?
     !latitude.blank? && !longitude.blank?
   end
