cbe56b547fa5b5cf339e48b5ffdd5eb3586d0cbb
Paolo Zaccagnini <paozac@gmail.com>
2012-03-12 13:58:20 +0100

    smtp: fix certificate error

--- config/initializers/mailer_settings.rb
+++ config/initializers/mailer_settings.rb
@@ -9,3 +9,9 @@ end
 if APP_NAME
   ActionMailer::Base.prepend_view_path("app/views/" + APP_NAME)
 end
+
+
+ActionMailer::Base.smtp_settings = {
+  # Postfix fails on production without this
+  :enable_starttls_auto => false
+}
