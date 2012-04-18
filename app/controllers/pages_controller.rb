d10e7ef00002e82b08d9298d46e359199cbef048
Paolo Zaccagnini <paozac@gmail.com>
2012-04-18 09:52:44 +0200

    added stats page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
   before_filter :load_question_set
-  
+
   def home
     @intro_data = ApiClient.new.intro_page_data rescue {}
   end
-  
+
   def info
     @popup = Popup.find_by_code(params[:code])
     render :layout => false if request.xhr?
+  end
+
+  def stats
   end
 end
