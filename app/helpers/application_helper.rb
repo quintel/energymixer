1df86ccd7f352cf532ddb5784dea5e871a497747
Paolo Zaccagnini <paozac@gmail.com>
2012-04-18 12:22:20 +0200

    sorting table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,16 @@ module ApplicationHelper
   def page_title(t)
     content_for(:page_title) { t }
   end
-  
+
   def ipad?
     request.env['HTTP_USER_AGENT'].downcase.index('ipad')
   rescue
     false
+  end
+
+  def sortable_header(name, field = nil)
+    field ||= name
+    order = params[:order] == 'asc' ? 'desc' : 'asc'
+    link_to name, {:sort => field, :order => order}
   end
 end
