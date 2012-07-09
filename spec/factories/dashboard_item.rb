3a967cf936c31f6deb183725afb94560f17abd70
Anthony Williams <hi@antw.me>
2012-07-09 18:39:17 +0100

    Test dashboard steps JSON.

--- spec/factories/dashboard_item.rb
+++ spec/factories/dashboard_item.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :dashboard_item do |f|
     f.gquery { generate(:dashboard_gquery) }
     f.label 'label'
-    f.steps '-0.5,0.0,0.5'
+    f.steps { "-0.#{ rand(7) + 1 },0.0,0.#{ rand(7) + 1 }" }
     f.association :question_set, factory: :question_set
   end
 
--- spec/integration/scenarios_spec.rb
+++ spec/integration/scenarios_spec.rb
@@ -4,6 +4,13 @@ feature 'A new scenarios' do
 
   let!(:question_set) { create :full_question_set }
 
+  def global_json
+    source = page.find('script#global_json').text
+    source = source.gsub("\nvar globals = ", '')
+
+    JSON.parse(source)
+  end
+
   # --------------------------------------------------------------------------
 
   scenario 'Shows the question set form' do
@@ -27,6 +34,18 @@ feature 'A new scenarios' do
     page.should have_css(".answer input#answer_#{aids[1]}")
     page.should have_css(".answer input#answer_#{aids[2]}")
     page.should have_css(".answer input#answer_#{aids[3]}")
+
+    # Make sure the correct dashboard steps were used.
+
+    steps = global_json['chart']['dashboard_steps']
+
+    steps.should have(2).elements
+
+    steps[ question_set.dashboard_items[0].gquery ].should \
+      eql( question_set.dashboard_items[0].steps.split(',').map(&:to_f) )
+
+    steps[ question_set.dashboard_items[1].gquery ].should \
+      eql( question_set.dashboard_items[1].steps.split(',').map(&:to_f) )
   end
 
   scenario 'Does not show questions from other question sets' do
@@ -38,6 +57,18 @@ feature 'A new scenarios' do
 
     page.should have_css('.question_tab', count: 2)
     page.should have_css('.answer input', count: 4)
+
+    # Make sure the correct dashboard steps were used.
+
+    steps = global_json['chart']['dashboard_steps']
+
+    steps.should have(2).elements
+
+    steps[ question_set.dashboard_items[0].gquery ].should \
+      eql( question_set.dashboard_items[0].steps.split(',').map(&:to_f) )
+
+    steps[ question_set.dashboard_items[1].gquery ].should \
+      eql( question_set.dashboard_items[1].steps.split(',').map(&:to_f) )
   end
 
 
