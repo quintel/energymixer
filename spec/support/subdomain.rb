97494ed75e530355d11437817b59741f54ca6118
Anthony Williams <hi@antw.me>
2012-07-09 14:47:04 +0100

    Use a real question set in specs.

--- spec/support/subdomain.rb
+++ spec/support/subdomain.rb
@@ -1,9 +1,12 @@
 module SubdomainSpec
   def self.included(base)
     base.instance_eval <<-RUBY
+      let(:default_question_set) do
+        create :question_set, name: 'gasmixer'
+      end
+
       before(:each) do
-        # request.expects(:subdomain).at_least_once.returns('gasmixer')
-        request.host = 'gasmixer.test.host'
+        request.host = '%s.test.host' % default_question_set.name
       end
     RUBY
   end
