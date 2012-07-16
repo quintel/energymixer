7f7d7ef77632aebf4ef8a09355c7685eae52443e
Anthony Williams <hi@antw.me>
2012-07-16 19:50:57 +0100

    Test that searches don't show scenarios from other partitions.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,21 +21,61 @@ Spork.prefork do
     # config.mock_with :rr
     # config.mock_with :rspec
 
+    # We apparently cannot add a before filter here, since request and
+    # controller aren't available. Yet they are if the filter is added to the
+    # example block. Odd!
+    config.include SubdomainSpec, type: :controller
+
+    # Handle `it('thing', :js)` as `it('thing', js: true)`.
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+
+    # Languages
+    # ---------
+
+    # Default all test runs to use English; you can change this in each
+    # individual test, or in a before(:each) block.
+    #
+    config.before(:each) { I18n.locale = 'en' }
+
+    # Database
+    # --------
+
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+    config.use_transactional_fixtures = false
     config.include FactoryGirl::Syntax::Methods
 
-    # We apparently cannot add a before filter here, since request and
-    # controller aren't available. Yet they are if the filter is added to the
-    # example block. Odd!
-    config.include SubdomainSpec, type: :controller
+    # Integration tests should use truncation; real requests aren't wrapped
+    # in a transaction, so neither should high-level tests. These filters need
+    # to be above the filter which starts DatabaseCleaner.
 
-    # Capybara.default_host = 'gasmixer.mixer.dev'
+    config.before(:each, type: :request) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    config.after(:each, type: :request) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    # The database_cleaner gem is used to restore the DB to a clean state before
+    # each example runs. This is used in preference over rspec-rails'
+    # transactions since we also need this behaviour in Cucumber features.
+
+    config.before(:suite) { DatabaseCleaner.strategy = :transaction }
+    config.before(:each)  { DatabaseCleaner.start                   }
+    config.after(:each)   { DatabaseCleaner.clean                   }
+
+    # Capybara
+    # --------
+
+    Capybara.javascript_driver = :webkit
+    Capybara.server_port = '54163'
+    Capybara.app_host    = 'http://gasmixer.mixer.dev:54163'
+
   end
 end
 
