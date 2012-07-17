7dd9051aeb626078c25521a9ef142483ba809d36
Anthony Williams <hi@antw.me>
2012-07-17 21:20:44 +0100

    Add "partition switcher" field to admin layout.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+
+$( function() {
+    // Partition switcher.
+    var field = $('header nav #partition select');
+
+    field.change( function(event) {
+        var splitHost, hostWithoutSubdomain;
+
+        splitHost            = window.location.host.split('.');
+        hostWithoutSubdomain = splitHost.slice(1, splitHost.length).join('.');
+
+        window.location.href =
+            window.location.protocol + '//' +
+            field.val() + '.' + hostWithoutSubdomain + '/admin';
+
+        event.preventDefault();
+    } );
+} );
