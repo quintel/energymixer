93ee97b4c551c39e3b20d72020433fc094d514c1
Anthony Williams <hi@antw.me>
2012-07-19 20:54:01 +0100

    Allow partitions to specify supported locales.
    Removes the "default_locale" and "multilanguage" configuration options
    in favour of "locales". This should be an array of two-character
    locales which are available to visitors. The first entry will be the
    default locale. Each partition may specify its own set of locales.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,18 @@ module ApplicationHelper
     order = params[:order] == 'asc' ? 'desc' : 'asc'
     link_to name.html_safe, params.merge({:sort => field, :order => order})
   end
+
+  # @return [String, nil]
+  #   Returns links to the current page in the other supported locales, or nil
+  #   if the partition does not support any others.
+  #
+  def other_locale_links
+    return nil unless partition.multi_language?
+
+    others = partition.other_locales(I18n.locale)
+    links  = others.map { |lang| link_to h(lang.to_s.upcase), locale: lang }
+
+    links.join("\n").html_safe
+  end
+
 end
--- spec/models/partition_spec.rb
+++ spec/models/partition_spec.rb
@@ -4,9 +4,9 @@ describe Partition do
   let(:attributes) { {
     hostname:             'testapp.example.org',
     max_total_cost:       150,
+    locales:              %w( en nl ),
     api_session_settings: { end_year: 2050 },
     default_locale:       'en',
-    multilanguage:        false,
     score:                false,
     google_analytics_key: '123'
   } }
@@ -17,16 +17,19 @@ describe Partition do
   its(:host)            { should eql('testapp.example.org') }
   its(:max_cost)        { should eql(150)                   }
   its(:api_settings)    { should eql(end_year: 2050)        }
-  its(:default_locale)  { should eql('en')                  }
+  its(:locales)         { should eql([:en, :nl])            }
+  its(:default_locale)  { should eql(:en)                   }
   its(:analytics_key)   { should eql('123')                 }
 
-  its(:multi_language?) { should be_false                   }
+  its(:multi_language?) { should be_true                    }
   its(:analytics?)      { should be_true                    }
   its(:show_score?)     { should be_false                   }
 
-  it 'should be multi-language by default' do
-    partition = Partition.new('name', attributes.except(:multilanguage))
-    partition.multi_language?.should be_true
+  it 'should not be multi-language with only one locale' do
+    attributes[:locales] = %w( nl )
+
+    partition = Partition.new('name', attributes)
+    partition.multi_language?.should be_false
   end
 
   it 'should disable analytics by default' do
