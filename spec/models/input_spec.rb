0e459c8e0101f9dac819870bd9c43d362cddc778
Paolo Zaccagnini <paozac@gmail.com>
2012-11-06 16:20:47 +0100

    fixed tests

--- spec/models/input_spec.rb
+++ spec/models/input_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Input do
-  it { should validate_presence_of :slider_id }
+  it { should validate_presence_of :key }
+  it { should validate_presence_of :value }
 end
 
