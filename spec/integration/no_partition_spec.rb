27f9a8d836633f3e6211fdd14669f9761042aa64
Paolo Zaccagnini <paozac@gmail.com>
2012-11-08 09:55:10 +0100

    catch exceptions on bad partition
    Too many exceptions coming from bots

--- spec/integration/no_partition_spec.rb
+++ spec/integration/no_partition_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'When no matching partition exists' do
   scenario 'Visiting the root page' do
-    running_this = -> { visit 'http://invalid.example.com/' }
-    expect(&running_this).to raise_error(/no such partition/i)
+    visit 'http://invalid.example.com/'
+    expect(page).to have_content('Bad partition')
   end
 end
