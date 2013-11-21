879a62e8f1d5bfd6a04bf8ff654cdcfa6863e10f
Anthony Williams <hi@antw.me>
2013-11-21 19:46:12 +0000

    Use digest URLs for assets
    Ensures that browsers don't serve up cached stylesheets after a deploy.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,9 @@ EnergyMixer::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
